207d0c16ee03cb0a24d8d54a89a33ee262c1bd5f
Erlanda Galant Prasetio <erland4512@gmail.com>
2025-07-09 22:43:53 +0700

    Fix routing and navigation for Netlify deployment

--- next.config.js
+++ next.config.js
@@ -1,37 +1,61 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  trailingSlash: true,
+  reactStrictMode: true,
+  swcMinify: true,
+  trailingSlash: false,
+  basePath: '',
   assetPrefix: process.env.NODE_ENV === 'production' ? '/' : '',
   images: {
     unoptimized: true,
     domains: [
-      "source.unsplash.com",
-      "images.unsplash.com",
-      "ext.same-assets.com",
-      "ugc.same-assets.com",
+      'source.unsplash.com',
+      'images.unsplash.com',
+      'ext.same-assets.com',
+      'ugc.same-assets.com',
     ],
     remotePatterns: [
       {
-        protocol: "https",
-        hostname: "source.unsplash.com",
-        pathname: "/**",
+        protocol: 'https',
+        hostname: 'source.unsplash.com',
+        port: '',
+        pathname: '/**',
       },
       {
-        protocol: "https",
-        hostname: "images.unsplash.com",
-        pathname: "/**",
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+        port: '',
+        pathname: '/**',
       },
       {
-        protocol: "https",
-        hostname: "ext.same-assets.com",
-        pathname: "/**",
+        protocol: 'https',
+        hostname: 'ext.same-assets.com',
+        port: '',
+        pathname: '/**',
       },
       {
-        protocol: "https",
-        hostname: "ugc.same-assets.com",
-        pathname: "/**",
+        protocol: 'https',
+        hostname: 'ugc.same-assets.com',
+        port: '',
+        pathname: '/**',
       },
     ],
+  },
+  async rewrites() {
+    return [
+      {
+        source: '/:path*',
+        destination: '/',
+      },
+    ];
+  },
+  async redirects() {
+    return [
+      {
+        source: '/landing',
+        destination: '/',
+        permanent: true,
+      },
+    ];
   },
 };
 
